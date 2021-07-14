b816c27143bb5ca95567f00a7f155ad807272b53
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-07-14 14:43:45 +0200

    Zoom effect

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,8 +1,9 @@
 uniform sampler2D uTexture;
+uniform float uProgress;
 varying vec2 vUv;
 
 void main(){
     vec4 myimage = texture(uTexture, vUv);
-    gl_FragColor = vec4(1., 1., 0., 1.);
+    gl_FragColor = vec4(uProgress, 0., 0., 1.);
     gl_FragColor = myimage;
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,5 +1,18 @@
 varying vec2 vUv;
+uniform float time;
+uniform float uProgress;
+uniform vec2 uResolution;
+uniform vec2 uQuadSize;
+
 void main(){
     vUv = uv;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+    vec4 defaultState = modelMatrix*vec4( position, 1.0 );
+    vec4 fullScreenState = vec4(position, 1.0);
+
+    fullScreenState.x *= uResolution.x/uQuadSize.x;
+    fullScreenState.y *= uResolution.y/uQuadSize.y;
+
+    vec4 finalState = mix(defaultState, fullScreenState, uProgress);
+
+    gl_Position = projectionMatrix * viewMatrix * finalState;
 }
